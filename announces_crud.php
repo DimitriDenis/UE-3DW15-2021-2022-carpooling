7fdf5a9a2006adaba4f4f9cda31b3501a1df9e36
Adrien Marques <adrien.marques@etu.unilim.fr>
2021-11-18 16:43:47 +0100

    main: MAJ base

---
+++ announces_crud.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="utf-8">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Page de modification des annonces</title>
+</head>
+<?
---
+++ cars_crud.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="utf-8">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Page de modification des voitures</title>
+</head>
+<?
